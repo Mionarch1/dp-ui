c4c26dbc60b8bb9f45cee98ccd266a3a5e4e5436
pingan <1136913663@qq.com>
2022-09-07 16:30:09 +0800

    feat: add icon docs

---
+++ docs/views/guide/icon/IconCard.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="icon-card">
+    <i class=""></i>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  name: { type: String, default: '' },
+  icon: { type: String, default: '' }
+});
+</script>
+
+<style lang="scss" scoped></style>
